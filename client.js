db998cfff26712578ce2d19221a0c5b93182814d
ashraful-commits <ashrfulalam10000@gmail.com>
2024-04-09 16:42:43 +0600

    final update

--- client.js
+++ client.js
@@ -28,6 +28,8 @@ document.addEventListener("DOMContentLoaded", () => {
     const outgoingListFontColor = urlParams.get('outgoingListFontColor') || 'white';
     const iconBgColor = urlParams.get('iconBgColor') || 'purple';
     const iconColor = urlParams.get('iconColor') || 'white';
+    const bodyWidth = urlParams.get('bodyWidth') || '50px';
+    const bodyHeight = urlParams.get('bodyHeight') || '50px';
 
     // Set colors dynamically
     setElementColor('.chatbot header', chatHeaderBgColor, chatHeaderFontColor);
@@ -81,6 +83,13 @@ document.addEventListener("DOMContentLoaded", () => {
     const closeBtn = document.querySelector(".close-btn");
     const chatbox = document.querySelector(".chatbox");
 
-    closeBtn.addEventListener("click", () => document.body.classList.remove("show-chatbot"));
-    chatbotToggler.addEventListener("click", () => document.body.classList.toggle("show-chatbot"));
+    closeBtn.addEventListener("click", () => {document.body.classList.remove("show-chatbot")
+    document.body.style.width="0px"
+    document.body.style.height="0px"
+});
+    chatbotToggler.addEventListener("click", () =>{ document.body.classList.toggle("show-chatbot")
+    document.body.style.width=bodyWidth
+    document.body.style.height=bodyHeight
+
+});
 });
