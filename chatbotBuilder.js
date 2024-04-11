da539a3b81a75fb1a5cbb90cb292aa6198076ac3
ashraful-commits <ashrfulalam10000@gmail.com>
2024-04-11 21:54:15 +0600

    upated

--- chatbotBuilder.js
+++ chatbotBuilder.js
@@ -8,7 +8,7 @@ function generateEmbedCode(prompt, bgColor, color, firstMessage) {
   const encodedFirstMessage = encodeURIComponent(firstMessage);
   const embedCode = `
   <iframe id="iframe" src="${chatbotUrl}?prompt=${prompt}&bgColor=${encodedBgColor}&color=${encodedColor}&message=${encodedFirstMessage}" 
-  style="pointer-events: auto; position: fixed; bottom: 0; right: 0; height:850px; width:560px; z-index:999999 ; padding: 100px 50px; "></iframe>
+  style="pointer-events: auto; position: fixed; bottom: 0; right: 0; height:850px; width:560px; z-index:999999 ; padding: 100px 50px; border-radius:none"></iframe>
   <button id="button" style="position: fixed; bottom: 50px; right: 50px; border-radius: 100%; padding: 10px; width: 50px; height: 50px; border: none; background: ${bgColor}; color: ${color}; display: flex; justify-content: center;align-items: center; z-index: 999999;" onclick="toggleIframe()">
   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-message-square">
     <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"/>
