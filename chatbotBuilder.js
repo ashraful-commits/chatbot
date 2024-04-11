90e709c75baad898d4c1520b2b1a7374e53a255c
ashraful-commits <ashrfulalam10000@gmail.com>
2024-04-11 06:13:58 +0600

    updated

---
+++ chatbotBuilder.js
@@ -0,0 +1,24 @@
+// chatbotBuilder.js
+
+function generateEmbedCode(prompt, color, firstMessage) {
+  const chatbotUrl = "YOUR_CHATBOT_URL";
+  const encodedPrompt = encodeURIComponent(prompt);
+  const encodedColor = encodeURIComponent(color);
+  const encodedFirstMessage = encodeURIComponent(firstMessage);
+
+  const embedCode = `
+      <iframe src="${chatbotUrl}?prompt=${encodedPrompt}&color=${encodedColor}&message=${encodedFirstMessage}" 
+          width="100%" height="300px" frameborder="0"></iframe>
+  `;
+  
+  return embedCode;
+}
+
+function onSubmitForm() {
+  const prompt = document.getElementById('prompt').value;
+  const color = document.getElementById('color').value;
+  const firstMessage = document.getElementById('firstMessage').value;
+
+  const embedCode = generateEmbedCode(prompt, color, firstMessage);
+  document.getElementById('embedCode').value = embedCode;
+}
