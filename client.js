79321d7d0bb01edc5bc9828e24822070b569e640
ashraful-commits <ashrfulalam10000@gmail.com>
2024-04-13 18:49:02 +0600

    updated

--- client.js
+++ client.js
@@ -35,7 +35,6 @@ document.addEventListener("DOMContentLoaded", function() {
                 const incomingChatLi = createChatLi("Thinking...", "incoming");
                 chatbox.appendChild(incomingChatLi);
                 chatbox.scrollTo(0, chatbox.scrollHeight);
-              
                     generateResponse(incomingChatLi, userMessage,config.prompt);
                 
             }, 600);
